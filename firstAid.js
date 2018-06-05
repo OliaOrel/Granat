06af5b7850e1662ac088885048800e0f3bb979b2
OliaOrel <olia.orel13@gmail.com>
2018-06-05 23:26:33 +0300

    Add interaction with the client

--- firstAid.js
+++ firstAid.js
@@ -1,5 +1,9 @@
 'use strict';
 
+const mod = require("./tree.js");
+const Node = mod.Node;
+const Tree = mod.Tree;
+
 const data = JSON
   .parse(require('fs')
   .readFileSync(__dirname + '/info.json'));
@@ -7,38 +11,29 @@ const answer = data;
 
 const TelegramBot = require('node-telegram-bot-api');
 const token = '582454263:AAGz7uCMCWIvPD6BNO7lq-2DaS_MOuw5dD4';
-const bot = new TelegramBot(token, {polling: true});
+const bot = new TelegramBot(token, { polling: true });
 
-// const answer = 'Приложите подорожник.';
+const tree = new Tree("Что у Вас случилось?\nВыберите из списка:");
+tree.build(answer, tree.root);
 
-let userId;
+let cur = tree.root;
 
-bot.onText(/\/start/, function(msg) {
-  userId = msg.from.id;
-  bot.sendMessage(userId, "Что у Вас случилось?\nВыберите из списка:");
-  choice(0, answer, (key) => {
-    bot.sendMessage(userId, key);
-    console.log(key);
-  });
-});
-
-function choice(i, obj, callback) {
-  const keys = Object.keys(obj);
-  let key = keys[i];
-  if (typeof callback === 'function') {
-    callback(key);
+bot.onText(/(.*)/, function(msg) {
+  const userId = msg.from.id;
+  if (msg.text === "\/start") {
+    cur = tree.root;
+    tree.getChildren(cur, (str) => bot.sendMessage(userId, str));
+  } else if (msg.text === "\/up") {
+    if (cur.parent) {
+      cur = cur.parent;
+      tree.getChildren(cur, (str) => bot.sendMessage(userId, str));
+    }
+  } else {
+    if (tree.findByIndex(cur, msg.text)) {
+      cur = tree.findByIndex(cur, msg.text);
+      tree.getChildren(cur, (str) => bot.sendMessage(userId, str));
+    } else {
+      bot.sendMessage(userId, "Не найдено :(")
+    }
   }
-  i++;
-  if (i < keys.length) {
-    choice(i, obj, (key) => {
-      bot.sendMessage(userId, key);
-      console.log(key);
-    });
-  };
-};
-
-
-
-bot.onText(/(.*[^/start])/, function(msg) {
-  bot.sendMessage(userId, "Some words");
 });
